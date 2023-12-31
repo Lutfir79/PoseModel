b64cd3d022dc640b52e0973223ea0022f71fb723
lufti <105582886+Lutfir79@users.noreply.github.com>
2023-12-31 13:44:00 +0800

    Update PoseModule.py
    add angle function

--- PoseModule.py
+++ PoseModule.py
@@ -42,31 +42,31 @@ class poseDetector():
                 if draw:
                     cv2.circle(img, (cx,cy), 5, (255,0,0), cv2.FILLED)
         return lmList
+        
+    def findAngle(self, img, p1, p2, p3, draw=True):
 
-# example project
-def main():
-    cap = cv2.VideoCapture(0)
-    pTime = 0
-    detector = poseDetector()
-    while True:
-        success, img = cap.read()
-        img = detector.findPose(img)
-        lmList = detector.findPosition(img)
-        print(lmList[2])
-        cv2.circle(img, (lmList[2][1], lmList[2][2]), 15, (0, 0, 255), cv2.FILLED)
-        # PUT FPS TO
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
+        # Get landmark
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
 
-        cv2.putText(img, str(int(fps)), (78, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        # Calculate Angle
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))
+        if angle < 0:
+            angle += 360
 
-        cv2.imshow("Imager", img)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
+        print(angle)
 
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+        #Draw
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x2, y2), (x3, y3), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 0), 2)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 0), 2)
+            cv2.circle(img, (x3, y3), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (255, 0, 0), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
