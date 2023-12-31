baf7d042051bdac599560209d84d661e602fff03
lufti <105582886+Lutfir79@users.noreply.github.com>
2023-12-31 13:45:16 +0800

    Add files via upload
    TrainerArchery project

---
+++ TrainerArchery.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import time
+import PoseModule as pm
+
+cap = cv2.VideoCapture('C:/Users/User/Desktop/Videoarcher/Cropped/Train/1.mp4')
+detector = pm.poseDetector()
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (940, 600))
+    img = detector.findPose(img, False)
+    lmList = detector.findPosition(img, False)
+    print(lmList)
+    if len(lmList) != 0:
+        # RIGHT ARM
+        detector.findAngle(img, 12, 14, 16)
+        # LEFT ARM
+        detector.findAngle(img, 11, 13, 15)
+
+    print(lmList)
+    cv2.imshow("Result", img)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cv2.destroyAllWindows()
